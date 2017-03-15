2c9e0441b9e86f6bcb87ae8b429ff0d6ecb421cd
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-15 13:52:16 -0600

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,38 @@ def copyright_generator
 	def active? path 
 		"active" if current_page? path
 	end
+
+	def alerts
+		alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+		if alert 
+			alert_generator alert
+		end
+	end 
+
+	def alert_generator msg
+		js add_gritter(msg, title: "Cody Higgins Portfolio", sticky: false, time: 1500)
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
